95456668a2e4ea189ab9204721a53a81a63e7550
Kento75 <kento2github@gmail.com>
2018-10-14 20:25:32 +0900

    Add vue-router

---
+++ learn-vue-router/src/page1.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>
+      page1
+    </p>
+    <router-link to="/page2">Go to page2</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+<style>
+</style>
---
+++ learn-vue-router/src/page2.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>
+      page2
+    </p>
+  </div>
+</template>
+
+<script>
+</script>
+<style>
+</style>
