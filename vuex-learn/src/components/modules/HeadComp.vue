ccb9be9c3666f313fcaac6eca46803b6d646d8ef
Kento75 <kento2github@gmail.com>
2018-10-23 06:25:09 +0900

    Add vuex learn

---
+++ vuex-learn/src/components/modules/HeadComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ title }}</div>
+</template>
+
+<script>
+export default {
+  name: 'headComp',
+  data () {
+    return {
+      title: '感想を入力'
+    }
+  }
+}
+</script>
---
+++ vuex-learn/src/components/modules/TextareaComp.vue
@@ -0,0 +1,26 @@
+<!-- htmlかくとこ -->
+<template>
+  <div>
+    <p class="error">{{ error }}</p>
+    <textarea></textarea>
+  </div>
+</template>
+
+<!-- スクリプトかくとこ -->
+<script>
+export default {
+  name: 'textareaComp',
+  data() {
+    return {
+      error: '入力は必須です'
+    }
+  }  
+}
+</script>
+
+<!-- cssかくとこ -->
+<style scoped>
+.error {
+  color: red;
+}
+</style>
